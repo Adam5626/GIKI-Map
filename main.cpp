27171a4ef536704deb29eccf93bee81b148f4222
Muhammad Adam Ch <82668601+Adam5626@users.noreply.github.com>
2022-01-06 05:50:02 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+//Name : Muhammad Adam Ch
+//Reg No : 2020261
+//BSAI
+//GIKI MAP
+
+
+#include "GIKIGuide.h";
+using namespace Guide;
+
+
+int main()
+{
+
+	GIKIMAP G; //Making object of class GIKIMAP
+	string start, end;
+
+	G.show_loctions(); //Displaying all the available location points
+	cout << endl << endl;
+	cout << "Enter Your Starting Point\n";
+	cin >> start;
+	cout << "Enter Your Destination\n";
+	cin >> end;
+	cout << endl << endl;
+
+	cout << "Path : ";
+	G.shortest_path(start, end); //Displaying the path along with the distance
+
+	return 0;
+}
